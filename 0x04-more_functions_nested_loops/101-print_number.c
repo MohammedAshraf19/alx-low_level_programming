1e2aa050cfc4f75b2535569cc70aa68aa814354c
Mohammed Ashraf <mohamedasharf55@gmail.com>
2023-03-24 15:51:48 -0700

    101-print_number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_number -  prints 10 times the numbers, from 0 to 14.
+* @n: number
+* Return: void.
+*/
+void print_number(int n)
+{
+		int re = n;
+
+		if (n < 0)
+		{
+			_putchar('-');
+			n = n * -1;
+			re = n;
+		}
+		re = re / 10;
+		if (re != 0)
+			print_number(re);
+		_putchar((n % 10) + '0');
+}
