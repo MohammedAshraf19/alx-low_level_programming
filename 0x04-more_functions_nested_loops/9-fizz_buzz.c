a9c008b2c310cc1f91384d28c84374bc50d4c1f3
Mohammed Ashraf <mohamedasharf55@gmail.com>
2023-03-24 13:10:33 -0700

    9-fizz_buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that  prints the numbers from 1 to 100.
+ *
+ * Return: Alawys 0 (Success)
+*/
+
+int main(void)
+{
+		int tmp = 1;
+
+		while (tmp <= 100)
+		{
+			if (tmp % 3 == 0 && tmp % 5 == 0)
+				printf("FizzBuzz ");
+			else if (tmp % 3 == 0)
+				printf("Fizz ");
+			else if (tmp % 5 == 0)
+			{
+				printf("Buzz ");
+			}
+			else
+				printf("%d ", tmp);
+			tmp++;
+		}
+		printf("\n");
+		return (0);
+}
