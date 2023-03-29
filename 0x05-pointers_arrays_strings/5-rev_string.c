076b8a6c7fcd43037fd6aedb4d0acecbae5a2cbc
Mohammed Ashraf <mohamedasharf55@gmail.com>
2023-03-28 19:06:01 -0700

    5-rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+* rev_string - reverse a string.
+* @s: a string.
+* Return: void.
+*/
+void rev_string(char *s)
+{
+		int tmp =  0;
+
+		int tmp2 = strlen(s) - 1;
+
+		while (tmp < strlen(s) / 2)
+		{
+			char swap = s[tmp];
+
+			s[tmp] = s[tmp2];
+			s[tmp2] = swap;
+			tmp++;
+			tmp2--;
+		}
+		_putchar('\n');
+}
