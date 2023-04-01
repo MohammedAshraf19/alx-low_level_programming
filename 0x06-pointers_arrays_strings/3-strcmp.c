a21bda86a63c54c677d9613903fff754e6b69b79
Mohammed Ashraf <mohamedasharf55@gmail.com>
2023-03-31 17:16:22 -0700

    3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,11 +8,17 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-		int i, result = 0;
+		int result = 0;
 
-		for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+		while (*s1)
 		{
-			result = ((int)(*s1 - 48) - ((int)*s2 - 48));
+			if (*s1 != *s2)
+			{
+				result = ((int)(*s1 - 48) - ((int)*s2 - 48));
+				break;
+			}
+			s1++;
+			s2++;
 		}
 		return (result);
 }
