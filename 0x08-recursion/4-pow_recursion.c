8a08d964a6994945aa2bb93cfbe3f7812a2b7c5a
Mohammed Ashraf <mohamedasharf55@gmail.com>
2023-04-05 18:40:13 -0700

    4-pow_recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* _pow_recursion -  Power of a given number.
+* @x: integer .
+* @y: integer.
+* Return: Integer.
+*/
+
+int _pow_recursion(int x, int y)
+{
+		int pow = 0;
+
+		if (y < 0)
+		{
+			return (-1);
+		}
+		else if (y == 0)
+		{
+			return (1);
+		}
+		else
+		{
+			pow = _pow_recursion(x, y - 1) * x;
+			return (pow);
+		}
+}
