b703f37dbd46c429bfcba5335c5965adcc2de192
Mohammed Ashraf <mohamedasharf55@gmail.com>
2023-04-15 12:28:42 -0700

    0-whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* main - check the code
-*
+* main - print program name.
+* @argc: integer.
+* @argv: list of string.
 * Return: Always 0.
 */
 
 int main(int argc, char **argv)
 {
-		printf("%s\n",argv[0]);
+		printf("%s\n", argv[0]);
 		(void) argc;
 		return (0);
 }
