17413e068a407c5ac104345724672f99e03098ff
Mohammed Ashraf <mohamedasharf55@gmail.com>
2023-04-15 07:05:52 -0700

    0-task

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+* _strstr -  locates a substring.
+* @haystack: string.
+* @needle: string.
+* Return: pointer.
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+		int i, k, tmp;
+
+		int found = 0;
+
+		for (i = 0; haystack[i] != '\0'; i++)
+		{
+			tmp = i;
+			for (k = 0; needle[k] != '\0'; k++)
+			{
+				if (needle[k] == haystack[tmp])
+				{
+					found = 1;
+				}
+				else
+				{
+					found = 0;
+					break;
+				}
+				tmp++;
+			}
+			if (found)
+				return (haystack + i);
+		}
+		return ('\0');
+}
