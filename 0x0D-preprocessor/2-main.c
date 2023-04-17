3c47f1d2e57709afeb80f472c1f7b34c58dff975
Mohammed Ashraf <mohamedasharf55@gmail.com>
2023-04-17 16:54:41 -0700

    Write a program that prints the name of the file it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define FILE_NAME (argv[0])
+
+/**
+* main - check the code
+* @argc: integer.
+* @argv: string.
+* Return: Always 0.
+*/
+
+int main(int argc, char **argv)
+{
+		printf("%s\n",FILE_NAME);
+		return (0);
+}
