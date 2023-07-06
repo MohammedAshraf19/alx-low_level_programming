7b674055ef00ac9873c6cff33e1d78374b88902d
Mohammed Ashraf <mohamedasharf55@gmail.com>
2023-07-06 08:37:12 -0700

    sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+* _sqrt_recursion -  the natural square root of a number.
+* @n: integer .
+* @num: integer.
+* Return: Integer.
+*/
+
+int square(int n, int num);
+int _sqrt_recursion(int n)
+{
+		return (square(n, 1));
+}
+
+/**
+* square -  get the natural square root of a number.
+* @n: integer .
+* @num: integer.
+* Return: Integer.
+*/
+
+int square(int n, int num)
+{
+		if (num * num == n)
+		{
+			return (num);
+		}
+		else if (num * num < n)
+		{
+			return (square(n, num + 1));
+		}
+		else
+			return (-1);
+}
+
