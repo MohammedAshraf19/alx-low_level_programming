17a40b45b4cb3f715e57a428c1b374d5bfa1d24d
Mohammed Ashraf <mohamedasharf55@gmail.com>
2023-07-11 23:56:47 -0700

    Write a function that returns a pointer to a newly allocated space in memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* _strdup - return string
+* @str: String.
+* Return: char pointer or NULL.
+*/
+char *_strdup(char *str)
+{
+		int str_size, i;
+
+		char* re;
+
+		if (str == NULL)
+		{
+			return (NULL);
+		}
+		for (i = 0; str[i] != '\0'; i++)
+		{
+		}
+		str_size = i;
+		re =  malloc(sizeof(char) * str_size);
+		if (re == NULL || str_size == 0)
+		{
+			return (NULL);
+		}
+		else
+		{
+			for (i = 0; i < str_size; i++)
+			{
+				re[i] = str[i];
+			}
+			return (re);
+		}
+}
