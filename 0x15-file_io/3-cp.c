30e529b3225fec44933feb20997ee7eb082b571e
Mohammed Ashraf <mohamedasharf55@gmail.com>
2023-08-08 07:35:26 -0700

    Write a program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,27 +12,27 @@ int main(int ac, char **av)
 {
 		int f_from, f_to;
 
-		size_t bytes = 0;
+		ssize_t bytes = 0;
 
 		char c[1024];
 
 		if (ac != 3)
-			dprintf("Usage: cp file_from file_to\n"), exit(97);
+			dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 		f_from = open(av[1], O_RDONLY);
 		if (f_from == -1)
-			dprintf("Error: Can't read from file %s\n", av[1]), exit(98);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 		f_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 		if (f_to == -1)
-			dprintf("Error: Can't write to %s\n", av[2]), exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 		while ((bytes = read(f_from, c, 1024)) > 0)
 			if (write(f_to, c, bytes) != bytes)
-				dprintf("Error: Can't write to %s\n", av[2]), exit(99);
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 		if (bytes == -1)
-			dprintf("Error: Can't read from file %s\n", av[1]), exit(98);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 		f_from = close(f_from), f_to = close(f_to);
 		if (f_from)
-			dprintf("Error: Can't close %d\n", f_from), exit(100);
+			dprintf(STDERR_FILENO, "Error: Can't close %d\n", f_from), exit(100);
 		if (f_to)
-			dprintf("Error: Can't close %d\n", f_to), exit(100);
+			dprintf(STDERR_FILENO, "Error: Can't close %d\n", f_to), exit(100);
 		return (0);
 }
