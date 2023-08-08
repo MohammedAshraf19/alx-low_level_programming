784680517a5f553e551305e59d81abd29c5c6320
Mohammed Ashraf <mohamedasharf55@gmail.com>
2023-08-07 19:19:49 -0700

    a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -35,7 +35,8 @@ int create_file(const char *filename, char *text_content)
 		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 		if (fd == -1)
 			return (-1);
-		bytes =  write(fd, text_content, len);
+		if (len)
+			bytes =  write(fd, text_content, len);
 		close(fd);
 		return (bytes == len ? 1 : -1);
 }
